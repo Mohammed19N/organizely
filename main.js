f0a981145c5af8386ccb7202ec41d537e71bb655
Mohammed Noaman <128026990+Mohammed19N@users.noreply.github.com>
2025-01-31 16:18:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+const clearButton = document.getElementById('clearButton');
 
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isDrawing) {
+        const currentX = e.offsetX;
+        const currentY = e.offsetY;
+
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(currentX, currentY);
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 2;
+        ctx.stroke();
+
+        lastX = currentX;
+        lastY = currentY;
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+canvas.addEventListener('mouseout', () => {
+    isDrawing = false;
+});
+
+
+clearButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
